3cc4a548d1be2629b1baf596a4c5edf7b66a5aee
Kam Ching <kam.ching@creapptive.com>
2020-07-22 16:36:34 +0800

    Add git-subrepo helper scripts

---
+++ git-subrepo-push.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#
+# Script to git-subrepo push subrepo directories to their remote master.
+#
+
+# array of subrepo directory
+IS_SYNC_ALL=false
+# at lease 1 argument
+NUM_ARGS=1
+
+# Function
+SCRIPT_NAME=${0##*/}
+Usage () {
+	echo
+	echo "Description:"
+	echo "Script to git-subrepo push subrepo directories to their remote master."
+	echo
+	echo "Usage: $SCRIPT_NAME [subrepo_dir...]"
+	echo "Options:"
+	echo " -a                           All subrepo directories"
+	echo " -h                           This help message"
+	echo
+}
+
+# Parse input argument(s)
+while [ "${1:0:1}" == "-" ]; do
+	OPT=${1:1:1}
+	case "$OPT" in
+	"a")
+		IS_SYNC_ALL=true
+		;;
+	"h")
+		Usage
+		exit
+		;;
+	esac
+	shift
+done
+
+SUBREPO_DIRS=$*
+
+if [ "$IS_SYNC_ALL" = false ] && [ "$#" -lt "$NUM_ARGS" ]; then
+    echo "Invalid parameter!"
+	Usage
+	exit 1
+fi
+
+if [ "$IS_SYNC_ALL" = true ]; then
+	echo "git-subrepo push to all..."
+    git-subrepo push --all
+else
+	echo "git-subrepo push to $SUBREPO_DIRS..."
+	dirs=($SUBREPO_DIRS)
+	for dir in "${dirs[@]}"; do
+		subrepo_dir=$(basename $dir)
+		git-subrepo push $subrepo_dir
+	done
+fi
