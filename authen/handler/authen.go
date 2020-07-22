5a461c6986ca04c9b5bddbfbda5dc46631337e13
Kam Ching <kam.ching@creapptive.com>
2020-07-22 15:39:49 +0800

    Implement all Authen.Login handler logics

--- authen/handler/authen.go
+++ authen/handler/authen.go
@@ -2,12 +2,14 @@ package handler
 
 import (
 	"context"
-
-	log "github.com/micro/go-micro/v2/logger"
+	"fmt"
 
 	authen "creapptive.com/ims-security/api/authen"
-	"creapptive.com/ims-security/api/message"
+	message "creapptive.com/ims-security/api/message"
+	"creapptive.com/ims-security/authen/config"
+	authenModel "creapptive.com/ims-security/authen/model"
 	"creapptive.com/ims-security/authen/repository"
+	"creapptive.com/ims-security/lib"
 )
 
 type Authen struct {
@@ -34,19 +36,80 @@ func (a *Authen) Close() error {
 }
 
 // Login is a single request handler called via client.Call or the generated client code
-func (a *Authen) Login(ctx context.Context, req *message.LoginRequest, rsp *authen.AuthenLoginReply) error {
-	log.Info("Received Authen.Login request")
+func (a *Authen) Login(ctx context.Context, req *message.LoginRequest, rsp *authen.AuthenLoginReply) (err error) {
+	// read the user's id, username and password
+	var user *authenModel.User
+	{
+		user, err = a.repo.ReadUserByUsername(req.Username)
+		if err != nil {
+			return fmt.Errorf("fail Authen.Login(%s): %w", req.Username, err)
+		}
+	}
+	// verify the user password
+	err = lib.ComparePassword(req.Password, user.Password)
+	if err != nil {
+		return fmt.Errorf("fail Authen.Login(%s): %w", req.Username, err)
+	}
+	rsp.Data = &authen.AuthUser{
+		Id:        user.ID.Hex(),
+		Username:  user.Username,
+		FailAt:    user.FailAt,
+		FailCount: user.FailCount,
+	}
 	return nil
 }
 
 // ChangePassword is a single request handler called via client.Call or the generated client code
-func (a *Authen) ChangePassword(ctx context.Context, req *message.ChangePasswordRequest, rsp *message.ChangePasswordReply) error {
-	log.Info("Received Authen.ChangePassword request")
+func (a *Authen) ChangePassword(ctx context.Context, req *message.ChangePasswordRequest, rsp *message.ChangePasswordReply) (err error) {
+	// read the user's id, username and password
+	var user *authenModel.User
+	{
+		user, err = a.repo.ReadUserByID(req.Id)
+		if err != nil {
+			return fmt.Errorf("fail Authen.ChangePassword(%s): %w", req.Id, err)
+		}
+	}
+	// verify the user password
+	err = lib.ComparePassword(req.OldPassword, user.Password)
+	if err != nil {
+		return fmt.Errorf("fail Authen.ChangePassword(%s): %w", req.Id, err)
+	}
+	// old password verified successfully, proceed to change password
+	password, err := lib.HashPassword(req.NewPassword, config.Config.Service.Authen.MoreOrLessCost)
+	if err != nil {
+		return fmt.Errorf("fail Authen.ChangePassword(%s): %w", req.Id, err)
+	}
+	user.Password = password
+	// update the user's password
+	_, err = a.repo.UpdatePasswordByID(user)
+	if err != nil {
+		return fmt.Errorf("fail Authen.ChangePassword(%s): %w", req.Id, err)
+	}
+	rsp.Data = req.Id
 	return nil
 }
 
 // ResetPassword is a single request handler called via client.Call or the generated client code
-func (a *Authen) ResetPassword(ctx context.Context, req *message.ResetPasswordRequest, rsp *message.ResetPasswordReply) error {
-	log.Info("Received Authen.ResetPassword request")
+func (a *Authen) ResetPassword(ctx context.Context, req *message.ResetPasswordRequest, rsp *message.ResetPasswordReply) (err error) {
+	// read the user's id, username and password
+	var user *authenModel.User
+	{
+		user, err = a.repo.ReadUserByID(req.Id)
+		if err != nil {
+			return fmt.Errorf("fail Authen.ResetPassword(%s): %w", req.Id, err)
+		}
+	}
+	// over-write the password with the default
+	password, err := lib.HashPassword(config.Config.Service.Authen.DefaultPassword, config.Config.Service.Authen.MoreOrLessCost)
+	if err != nil {
+		return fmt.Errorf("fail Authen.ResetPassword(%s): %w", req.Id, err)
+	}
+	user.Password = password
+	// update the user's password
+	_, err = a.repo.UpdatePasswordByID(user)
+	if err != nil {
+		return fmt.Errorf("fail Authen.ResetPassword(%s): %w", req.Id, err)
+	}
+	rsp.Data = req.Id
 	return nil
 }
